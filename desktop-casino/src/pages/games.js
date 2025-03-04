04434c8f662a88119a5d683d9c8b9b42932df46d
dimassxgod <dimonsky787@gmail.com>
2025-03-04 21:50:22 +0200

    Все с 0

--- desktop-casino/src/pages/games.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Games = () => {
-  return <h1>🎰 Список игр</h1>;
-};
-
-export default Games;
--- desktop-casino/src/pages/home.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home = () => {
-  return <h1>🏠 Добро пожаловать в казино!</h1>;
-};
-
-export default Home;
